6ee73460317b539e72528be30832c3d9ab4b8546
frankjay <frankjr212711@gmail.com>
2024-02-01 02:05:53 +0000

    mess

--- src/components/GjrHeroSection.js
+++ src/components/GjrHeroSection.js
@@ -1,83 +1,13 @@
 import { useState } from 'react'
 import './GjrHeroSection.css'
 
-export const GjrHeroSection = ({ slides }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const sliderStyles = {
-    height: '100%',
-    position: 'relative'
-}
-  const slideStyles = {
-    width: '100%',
-    height: '100%',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundImage: `url(${slides[currentIndex].url})`
-  }
-  const leftArrowStyles = {
-    position: 'absolute',
-    top: '50%',
-    left: '32px',
-    transform: 'translateY(0, -50%)',
-    fontSize: '45px',
-    color: '#fff',
-    zIndex: 1,
-    cursor: 'pointer'
-  }
-
-  const rightArrowStyles = {
-    position: 'absolute',
-    top: '50%',
-    right: '32px',
-    transform: 'translateY(-50%, 0%)',
-    fontSize: '45px',
-    color: '#fff',
-    zIndex: 1,
-    cursor: 'pointer'
-  }
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-  setCurrentIndex(newIndex)
-  }
+export const GjrHeroSection = () => {
   
-  const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex)
-  }
-  const dotsContainerStyles = {
-    display: 'flex',
-    justifyContent: 'center',
-  }
-  const dotStyles = {
-    margin: '0 3px',
-    cursor: 'pointer',
-    fontSize: '20px',
-  }
+
   return (
-    <div style={sliderStyles}>
-      <div>
-        <div className="fa fa-angle-left" style={leftArrowStyles} onClick={goToPrevious}></div>
-        <div className="fa fa-angle-right" style={rightArrowStyles} onClick={goToNext}></div>
+    <div >
+
+      
       </div>
-
-
-      <div style={slideStyles}></div>
-
-      <div style={dotsContainerStyles}>
-        {slides.map((slide, slideIndex) => {
-          <div key={slideIndex} style={dotStyles}>
-                ⚫️
-            
-          </div>
-        })}
-      </div>
-
-
-    
-  </div>
- 
   )
 }
--- src/components/NavbarGJR.js
+++ src/components/NavbarGJR.js
@@ -1,69 +1,59 @@
-import { GjrHeroSection } from "./GjrHeroSection";
+import { GjrHeroSection, Slider } from "./GjrHeroSection";
 import "./NavbarGJR.css";
 import { Link } from "react-router-dom";
 
 const containerStyles = {
-  width: '100%',          
-  margin: '0 auto',
-  gridRow: 'span 3'
+  width: "100%",
+  margin: "0 auto",
+  gridRow: "span 3",
 };
 
 export const NavbarGJR = () => {
-  const slides = [
-    { url: 'images/img-1.jpg', title: 'Beach' },
-    { url: 'images/img-2.jpg', title: 'Boat' },
-    { url: 'images/img-3.jpg', title: 'Forest' },
-    { url: 'images/img-4.jpg', title: 'City' },
-    { url: 'images/img-5.jpg', title: 'Italy' }
-  ];
-
+  const imgOne = "images/img-1.jpg";
+  const imgTwo = "images/img-2.jpg";
+  const imgThre = "images/img-4.jpg";
+  const imgFour = "images/img-4.jpg";
+  const imgFive = "images/img-5.jpg";
 
   return (
     <>
       <section className="nav__gjr_wrapper">
-      <div className="nav__gjr">
-        <div className="nav__content_gjr">
-          <div className="nav__banner_gjr">
-            <div className="logo_gjr">
-              {/* <img src="" alt="gjr_logo" /> */}
-              <i className="fas fa-snowflake"></i>
-            </div>
-            <h1 className="nav__title">
-              GJR
-              <span>Furniture</span>
+        <div className="nav__gjr">
+          <div className="nav__content_gjr">
+            <div className="nav__banner_gjr">
+              <div className="logo_gjr">
+                {/* <img src="" alt="gjr_logo" /> */}
+                <i className="fas fa-snowflake"></i>
+              </div>
+              <h1 className="nav__title">
+                GJR
+                <span>Furniture</span>
               </h1>
               <p>The Right Fit For Your Home & Office</p>
+            </div>
+
+            <ul>
+              <li>
+                <Link to="/gjr">Home</Link>
+              </li>
+              <li>
+                <Link>Kitchens</Link>
+              </li>
+              <li>
+                <Link>Wordrobes</Link>
+              </li>
+              <li>
+                <Link>TV Units</Link>
+              </li>
+              <li>
+                <Link>Vanity Units</Link>
+              </li>
+            </ul>
           </div>
-
-   
-
-          <ul>
-            <li>
-              <Link to="/gjr">Home</Link>
-            </li>
-            <li>
-              <Link>Kitchens</Link>
-            </li>
-            <li>
-              <Link>Wordrobes</Link>
-            </li>
-            <li>
-              <Link>TV Units</Link>
-            </li>
-            <li>
-              <Link>Vanity Units</Link>
-            </li>
-          </ul>
         </div>
-        </div>
-        <div style={containerStyles}>
-          <GjrHeroSection slides={slides } />
 
-        </div>
+        <GjrHeroSection />
       </section>
- 
-
-
     </>
   );
 };
