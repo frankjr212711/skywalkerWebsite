b6562705eaccd3b403aa46e6d774f95f2cefe171
frankjay <frankjr212711@gmail.com>
2024-02-01 19:14:39 +0000

    hero__container

--- src/components/pages/SkyHeroSection.js
+++ src/components/pages/SkyHeroSection.js
@@ -3,8 +3,8 @@ import './SkyHeroSection.css';
 
 export const SkyHeroSection = () => {
   return (
-    <>
+    <div className='hero__container'>
      <h1>Sky Hero Section</h1>
-    </>
+    </div>
   )
 }
