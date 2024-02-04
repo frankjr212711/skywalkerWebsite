4f5db02526a73a637d96fdc0c6fbac51c0139aff
frankjay <frankjr212711@gmail.com>
2024-02-04 05:51:54 +0000

    close_4_today

--- src/components/pages/GjrHeroSection.js
+++ src/components/pages/GjrHeroSection.js
@@ -1,61 +1,72 @@
-
 import "./GjrHeroSection.css";
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 
-import img1 from "../../assets/gjr-img-03.jpg";
+import img1 from "../../assets/gjr-img-01.jpg";
 import img2 from "../../assets/gjr-img-02.jpg";
-import img3 from "../../assets/gjr-img-01.jpg";
-import img4 from "../../assets/gjr-img-03.jpg";
-import img5 from "../../assets/gjr-img-02.jpg";
-
-
+import img3 from "../../assets/gjr-img-03.jpg";
+import img4 from "../../assets/gjr-img-04.jpg";
+import img5 from "../../assets/sky-img-abt-01.jpg";
 
 export const GjrHeroSection = () => {
-  const [open, setOpen] = useState(0);
+  const [active, setActive] = useState(false);
 
   const sliderContent = [
     {
-      img: img1, 
-      name: 'Wanda Maximoff',
+      img: img1,
+      name: "Wanda Maximoff",
     },
     {
-      img: img2, 
-      name: 'The Hulk',
+      img: img2,
+      name: "The Hulk",
     },
     {
-      img: img3, 
-      name: 'Iron Man',
+      img: img3,
+      name: "Iron Man",
     },
     {
-      img: img4, 
-      name: 'Black Panther',
+      img: img4,
+      name: "Black Panther",
     },
     {
-      img: img5, 
-      name: 'Black Panther',
-    }
-]
+      img: img5,
+      name: "Black Panther",
+    },
+  ];
 
-return (
-  <>
-   <div className="gjr_hero_wrapper">
+  const slide = (type) => {
+    let local;
+    if (type === 'next') {
+      local = active + 1;
+      setActive(local)
+    }
+    if (type === 'prev') {
+      local = active - 1;
+      setActive(local)
+    }
+}
+  return (
+    <>
+      <div className="gjr_hero_wrapper">
         <div className="gjr_hero_content">
-          {
-            sliderContent.map((slide, i) => {
-              return (
-                <img src={slide.img} key={i}  alt="slideImg_01" className={`${i === open ? "clip-visible": "clip-hidden"}`} />
-              )
-            })
-          }
+          {sliderContent.map((slide, i) => {
+            return (
+              <img
+                src={slide.img}
+                key={i}
+                alt="slideImg_01"
+                className={`${i === active ? "clip-visible" : "clip-hidden"}`}
+              />
+            );
+          })}
           <div>
-          <button>
-            <i className="fa fa-angle-left"></i>
+            <button id="back" onClick={() => slide('prev')}>
+              <i className="fa fa-angle-left"></i>
+            </button>
+            <button id="forward" onClick={() => slide('next')}>
+              <i className="fa fa-angle-right"></i>
             </button>
           </div>
-          
         </div>
-      
-     
       </div>
     </>
   );
